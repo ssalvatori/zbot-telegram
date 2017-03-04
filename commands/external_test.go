f7a869030f3bbfa1ced9fff7b80cede5e7306546
Stefano Salvatori <ssalvatori@gmail.com>
2017-03-04 16:33:07 -0300

    fix unit-test

--- commands/external_test.go
+++ commands/external_test.go
@@ -13,7 +13,7 @@ var externalCommand = ExternalCommand{
 func TestExternalCommandOK(t *testing.T) {
 	assert.Equal(t, "OK arg1 arg2 arg3\n", externalCommand.ProcessText("!test arg1 arg2 arg3", user), "external")
 	externalCommand.Next = &FakeCommand{}
-	assert.Equal(t, "", externalCommand.ProcessText("!date arg1 arg2", user), "external")
+	assert.Equal(t, "", externalCommand.ProcessText("!test arg1 arg2", user), "external")
 }
 
 func TestExternalCommandInject(t *testing.T) {
--- commands/ignore_test.go
+++ commands/ignore_test.go
@@ -11,18 +11,18 @@ var ignoreCommand = IgnoreCommand{
 }
 
 func TestIgnoreCommandHelp(t *testing.T) {
-	result := "*!ignore* Options available: \n list (show all user ignored) add <username> (ignore a user for 10 minutes)"
+	result := "*!ignore* Options available: \n list (show all user ignored) \n add <username> (ignore a user for 10 minutes)"
 	assert.Equal(t, result, ignoreCommand.ProcessText("!ignore help", user), "Ignore help")
 }
 
 func TestIgnoreCommandList(t *testing.T) {
 	ignoreCommand.Db = &db.MockZbotDatabase{
 		User_ignored: []db.UserIgnore{
-			{Username: "rigo", Since: "12", Until: "12"},
-			{Username: "jav", Since: "32", Until: "32"},
+			{Username: "rigo", Since: "12", Until: "22"},
+			{Username: "jav", Since: "32", Until: "42"},
 		},
 	}
-	expected := "[ @rigo ] since [12] until [12]/n[ @jav ] since [32] until [32]"
+	expected := "[ @rigo ] since [01-01-1970 00:00:12 UTC] until [01-01-1970 00:00:22 UTC]/n[ @jav ] since [01-01-1970 00:00:32 UTC] until [01-01-1970 00:00:42 UTC]"
 	assert.Equal(t, expected, ignoreCommand.ProcessText("!ignore list", user), "Last Command")
 }
 
@@ -52,6 +52,6 @@ func TestConvertDates(t *testing.T) {
 
 	sinceFormated, untilFormated := convertDates(since, until)
 
-	assert.Equal(t, "2017-03-04T13:21:20-03:00", sinceFormated, "format ok")
-	assert.Equal(t, "2017-03-04T13:31:20-03:00", untilFormated, "format ok")
+	assert.Equal(t, "04-03-2017 16:21:20 UTC", sinceFormated, "format ok")
+	assert.Equal(t, "04-03-2017 16:31:20 UTC", untilFormated, "format ok")
 }
